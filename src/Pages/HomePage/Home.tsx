bd8d2bc92d3fc50ec0c15856a11603f378dbf734
Elnaraa9 <7khtht3@code.edu.az>
2022-12-09 07:27:44 +0400

    uncompleted

--- src/Pages/HomePage/Home.tsx
+++ src/Pages/HomePage/Home.tsx
@@ -1,13 +1,15 @@
-import { ContactUs } from "./components/ContactUs/ContactUs"
-import { Hero } from "./components/Hero/Hero"
-import { TeamMember } from "./components/TeamMember/TeamMember"
+import { ContactUs } from "./components/ContactUs/ContactUs";
+import { Hero } from "./components/Hero/Hero";
+import { Projects } from "./components/Projects/Projects";
+import { TeamMember } from "./components/TeamMember/TeamMember";
 
-export const Home=()=>{
-    return(
-        <div className="home">
-            <Hero/>
-            <TeamMember/>
-            <ContactUs />
-        </div>
-    )
-}
+export const Home = () => {
+  return (
+    <div className="home">
+      <Hero />
+      <Projects />
+      <TeamMember />
+      <ContactUs />
+    </div>
+  );
+};
