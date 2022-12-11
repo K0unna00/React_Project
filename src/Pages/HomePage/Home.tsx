c9fe4152a639500af291df9515e4bc1746e16336
Baxtiyar <100702860+K0unna00@users.noreply.github.com>
2022-12-11 23:02:01 +0400

    FixingSomeErrors

--- src/Pages/HomePage/Home.tsx
+++ src/Pages/HomePage/Home.tsx
@@ -3,7 +3,6 @@ import { AboutUs } from './components/AboutUs/AboutUs';
 import { ClientsFeedback } from './components/ClientsFeedback/ClientsFeedback';
 import { ContactUs } from './components/ContactUs/ContactUs';
 import { Hero } from './components/Hero/Hero';
-import { Projects } from './components/Projects/Projects';
 import { TeamMember } from './components/TeamMember/TeamMember';
 
       
