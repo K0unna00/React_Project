9a4cbdea847fd0be6ce3287063535295a72c5e00
Elnaraa9 <7khtht3@code.edu.az>
2022-12-11 13:28:22 +0400

    responsive

--- src/Pages/HomePage/Home.tsx
+++ src/Pages/HomePage/Home.tsx
@@ -2,6 +2,7 @@ import { AboutUs } from './components/AboutUs/AboutUs';
 import { ClientsFeedback } from './components/ClientsFeedback/ClientsFeedback';
 import { ContactUs } from './components/ContactUs/ContactUs';
 import { Hero } from './components/Hero/Hero';
+import { Projects } from './components/Projects/Projects';
 import { TeamMember } from './components/TeamMember/TeamMember';
 
 export const Home = () => {
@@ -9,6 +10,7 @@ export const Home = () => {
     <div className="home">
       <Hero />
       <AboutUs />
+      <Projects />
       <TeamMember />
       <ClientsFeedback />
       <ContactUs />
