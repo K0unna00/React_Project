a5e8cb50c1d773629a27ef6ab50736b3bef6429e
Baxtiyar <100702860+K0unna00@users.noreply.github.com>
2022-12-23 00:46:05 +0400

    FixingsSomethings

--- src/Pages/RegisterPage/RegisterPage.tsx
+++ src/Pages/RegisterPage/RegisterPage.tsx
@@ -3,6 +3,7 @@ import * as yup from 'yup';
 import { SubmitHandler, useForm } from 'react-hook-form';
 import { baseURL } from '../../constants';
 import { yupResolver } from '@hookform/resolvers/yup';
+import { useNavigate } from 'react-router-dom';
 
 type RegisterFormData = {
   firstName: string;
@@ -27,6 +28,7 @@ const schema = yup
   .required();
 
 export const RegisterPage = () => {
+  const navigate = useNavigate();
   const {
     register,
     handleSubmit,
@@ -35,7 +37,6 @@ export const RegisterPage = () => {
     resolver: yupResolver(schema),
   });
   const onSubmit: SubmitHandler<RegisterFormData> = (data) => {
-    console.log(data);
     fetch(`${baseURL}/users/register`, {
       method: 'POST',
       body: JSON.stringify(data),
@@ -46,6 +47,7 @@ export const RegisterPage = () => {
       .then((response) => response.json())
       .then((data) => {
         alert(data);
+        navigate('/login');
       });
   };
   return (
